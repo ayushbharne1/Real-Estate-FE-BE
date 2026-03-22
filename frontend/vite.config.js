e05fad1ce3c9690a60636e91eccc0bc499985f3b
tichuu77 <akashtikhat50@gmail.com>
2026-03-22 16:14:06 +0530

    fix: fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
- plugins: [react(), tailwindcss()],  // remove the duplicate plugins line
+  plugins: [react(), tailwindcss()],  // remove the duplicate plugins line
   resolve: {
     dedupe: ['react', 'react-dom', 'react-router-dom'],  // add this
   },
@@ -13,8 +13,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) return 'react-vendor'
-          if (id.includes('node_modules/react-router')) return 'router-vendor'
+          if (id.includes('node_modules/react-router') ||
+            id.includes('node_modules/react') ||
+            id.includes('node_modules/react-dom')) return 'react-vendor'
           if (id.includes('node_modules/@reduxjs') || id.includes('node_modules/react-redux')) return 'redux-vendor'
           if (id.includes('node_modules/leaflet') || id.includes('node_modules/react-leaflet')) return 'map-vendor'
           if (id.includes('node_modules/@tanstack')) return 'table-vendor'
